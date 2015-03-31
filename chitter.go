2398405b00430299e1b9f4bc1aa40ba9eadb4b53
fuweixiao <weixiaofu2014@u.northwestern.edu>
2015-03-30 22:50:07 -0400

    parse string done

--- chitter.go
+++ chitter.go
@@ -4,24 +4,29 @@ import (
 	"fmt"
 	"net"
 	"os"
-)
-
-const (
-	CONN_HOST = "localhost"
-	CONN_PORT = "3333"
-	CONN_TYPE = "tcp"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	// Parse command line arguments
+	port := "9999"
+	if len(os.Args) == 1 {
+		println("Usage: port number required! \nExample: go run chitter.go 9999")
+		os.Exit(0)
+	} else {
+		port = os.Args[1]
+	}
 	// Listen for incoming connections.
-	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+
+	l, err := net.Listen("tcp", "localhost"+":"+port)
 	if err != nil {
-		fmt.Println("Error listening:", err.Error())
+		fmt.Println("Error listening:", err.Error()+port)
 		os.Exit(1)
 	}
 	// Close the listener when the application closes.
 	defer l.Close()
-	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+	fmt.Println("Listening on " + "localhost" + ":" + "3333")
 	for {
 		// Listen for an incoming connection.
 		conn, err := l.Accept()
@@ -40,12 +45,23 @@ func handleRequest(conn net.Conn) {
 	buf := make([]byte, 1024)
 	// Read the incoming connection into the buffer.
 	reqLen, err := conn.Read(buf)
-	for i := 0; i < reqLen; i++ {
-		fmt.Printf("%c", buf[i])
-	}
 	if err != nil {
 		fmt.Println("Error reading:", err.Error())
 	}
+
+	// Parse data
+	data := buf[0 : reqLen-1]
+	msg := string(data)
+	id := strings.Split(msg, ":")[0]
+	// Send messages
+	if _, err := strconv.Atoi(id); err == nil {
+		print(id)
+	} else if id == "whoami" {
+		print("Who cares who the fuck you are!")
+	} else if id == "all" {
+		print("all")
+	}
+
 	// Send a response back to person contacting us.
 	conn.Write([]byte("Message received."))
 	// Close the connection when you're done with it.
